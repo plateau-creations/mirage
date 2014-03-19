7079b580136c5beb773f9ffe92ee55d09af3e231
codextends <codextends@gmail.com>
2014-03-19 15:24:12 +0100

    Initial commit

---
+++ src/Plateau/Mirage/MirageServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Plateau\Mirage;
+
+use Illuminate\Support\ServiceProvider;
+
+class MirageServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('plateau/mirage');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
